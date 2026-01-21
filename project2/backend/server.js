23144670698cc738af1968147de0b1b5177a6d9e
raju barman <rajubarman7665@gmail.com>
2026-01-21 21:59:58 +0530

    backend updated

--- project2/backend/server.js
+++ project2/backend/server.js
@@ -1,11 +1,13 @@
-import jsonServer from "json-server";
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+const port = process.env.PORT || 5000;
+
 server.use(middlewares);
 server.use(router);
 
-server.listen(5000, () => {
-  console.log("JSON Server is running");
+server.listen(port, () => {
+  console.log("JSON Server is running on port " + port);
 });
